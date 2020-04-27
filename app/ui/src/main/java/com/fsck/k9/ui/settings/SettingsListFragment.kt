e6c87d39882985f632913adba0d19c7da6823fdf
BeiJingLgs <38711296+BeiJingLgs@users.noreply.github.com>
2020-04-27 15:07:51 +0800

    修改了添加账户半路退出配置，删除账户后不直接回到首页的问题

--- app/ui/src/main/java/com/fsck/k9/ui/settings/SettingsListFragment.kt
+++ app/ui/src/main/java/com/fsck/k9/ui/settings/SettingsListFragment.kt
@@ -1,6 +1,7 @@
 package com.fsck.k9.ui.settings
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,12 +24,14 @@ import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.fragment_settings_list.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.ArrayList
 
 class SettingsListFragment : Fragment(), ConfirmationDialogFragment.ConfirmationDialogFragmentListener {
     private val viewModel: SettingsViewModel by viewModel()
     private lateinit var accountSetting: AccountSettingsFragment
     private lateinit var settingsAdapter: GroupAdapter<ViewHolder>
     private val accountRemover: BackgroundAccountRemover by inject()
+    private val mAccounts: MutableList<Account> = mutableListOf()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_settings_list, container, false)
     }
@@ -61,7 +64,20 @@ class SettingsListFragment : Fragment(), ConfirmationDialogFragment.Confirmation
             if (accounts.isEmpty()) {
                 launchOnboarding()
             } else {
-                populateSettingsList(accounts)
+//                Toast.makeText(context, "看看有几个..1111" + accounts.size, Toast.LENGTH_SHORT).show()
+                mAccounts.clear()
+                for (account:Account in accounts) {
+                    val name = account.name
+                    Log.i("tag", "cccccccccc$name")
+                    if (account.toString() == null || name == "") {
+                        accountRemover.removeAccountAsync(account.uuid)
+                    }
+                    if (account.description != null) {
+                        mAccounts.add(account)
+                    }
+                }
+                Log.i("tag","BBBBBBBBccccccc"+mAccounts.size)
+                populateSettingsList(mAccounts)
             }
         }
     }
@@ -69,6 +85,7 @@ class SettingsListFragment : Fragment(), ConfirmationDialogFragment.Confirmation
     private fun populateSettingsList(accounts: List<Account>) {
         settingsAdapter.clear()
 //        Toast.makeText(context, "看看有几个.." + accounts.size, Toast.LENGTH_SHORT).show()
+        Log.i("tag","BBBBBBBBcccccccddd"+mAccounts.size)
 //        val generalSection = Section().apply {
 //            val generalSettingsActionItem = SettingsActionItem(
 //                    getString(R.string.general_settings_title),
